009e7bd3b4e9d1df0bc31d3555886ba44315a891
Juliano Canuto <julianocanuto@gmail.com>
2021-09-09 23:53:41 -0300

    Add Footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <div className="container">
         <h1 className="text-primary">Sales Management System</h1>
       </div>
+      <Footer />
     </>
   );
 }
