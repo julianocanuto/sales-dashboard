ba71c2ec68612b1b3c3a583b4274ac7aaf869d8d
Juliano Canuto <julianocanuto@gmail.com>
2021-09-12 19:18:55 -0300

    Update findAll from SellerService to return a list of SellerDTO

--- backend/src/main/java/br/com/julianocanuto/vendas/services/SellerService.java
+++ backend/src/main/java/br/com/julianocanuto/vendas/services/SellerService.java
@@ -1,10 +1,12 @@
 package br.com.julianocanuto.vendas.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.julianocanuto.vendas.dto.SellerDTO;
 import br.com.julianocanuto.vendas.entities.Seller;
 import br.com.julianocanuto.vendas.repositories.SellerRepository;
 
@@ -14,7 +16,8 @@ public class SellerService {
 	@Autowired
 	private SellerRepository repository;
 	
-	public List<Seller> findAll(){
-		return repository.findAll();
+	public List<SellerDTO> findAll(){
+		List<Seller> listOfSellersFromDB = repository.findAll();
+		return listOfSellersFromDB.stream().map(seller -> new SellerDTO(seller)).collect(Collectors.toList());
 	}
 }
