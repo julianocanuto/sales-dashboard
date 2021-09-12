fa3f3702f4e7f92a0553bbc8f153c551b2d1b2bd
Juliano Canuto <julianocanuto@gmail.com>
2021-09-12 20:53:17 -0300

    Update SaleService to be transactional read only and cache sellers using SellerRepository before query the sales

--- backend/src/main/java/br/com/julianocanuto/vendas/services/SaleService.java
+++ backend/src/main/java/br/com/julianocanuto/vendas/services/SaleService.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.julianocanuto.vendas.dto.SaleDTO;
 import br.com.julianocanuto.vendas.entities.Sale;
 import br.com.julianocanuto.vendas.repositories.SaleRepository;
+import br.com.julianocanuto.vendas.repositories.SellerRepository;
 
 @Service
 public class SaleService {
@@ -15,7 +17,12 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
 	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll();
 		Page<Sale> listOfSalesFromDB = repository.findAll(pageable);
 		return listOfSalesFromDB.map(sale -> new SaleDTO(sale));
 	}
