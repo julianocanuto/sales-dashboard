09ced498b6564bfbc8e5b6d89d6db25140724b9a
Juliano Canuto <julianocanuto@gmail.com>
2021-09-12 18:13:22 -0300

    Add Entity Seller

---
+++ backend/src/main/java/br/com/julianocanuto/vendas/entities/Seller.java
@@ -0,0 +1,36 @@
+package br.com.julianocanuto.vendas.entities;
+
+public class Seller {
+
+	private Long id;
+	private String name;
+	
+	public Seller() {
+		
+	}
+
+	public Seller(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
