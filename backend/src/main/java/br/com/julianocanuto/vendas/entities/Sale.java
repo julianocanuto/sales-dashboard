08d9a708e0db4654a00fce4de830717677310ea6
Juliano Canuto <julianocanuto@gmail.com>
2021-09-12 18:47:46 -0300

    Add object relational mapping

--- backend/src/main/java/br/com/julianocanuto/vendas/entities/Sale.java
+++ backend/src/main/java/br/com/julianocanuto/vendas/entities/Sale.java
@@ -2,14 +2,28 @@ package br.com.julianocanuto.vendas.entities;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_sales")
 public class Sale {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Integer visited;
 	private Integer deals;
 	private Double amount;
 	private LocalDate date;
 
+	@ManyToOne
+	@JoinColumn(name = "seller_id")
 	private Seller seller;
 
 	public Sale() {
--- backend/src/main/java/br/com/julianocanuto/vendas/entities/Seller.java
+++ backend/src/main/java/br/com/julianocanuto/vendas/entities/Seller.java
@@ -3,11 +3,23 @@ package br.com.julianocanuto.vendas.entities;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_sellers")
 public class Seller {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
+	@OneToMany(mappedBy = "seller")
 	private List<Sale> sales = new ArrayList<>();
 	
 	public Seller() {
