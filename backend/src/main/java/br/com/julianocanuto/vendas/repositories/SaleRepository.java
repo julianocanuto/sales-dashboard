8aed679b97e8996f127e5be53291127fa9911044
Juliano Canuto <julianocanuto@gmail.com>
2021-09-12 21:43:41 -0300

    Correct typo in custom query amount of sales grouped by seller

--- backend/src/main/java/br/com/julianocanuto/vendas/repositories/SaleRepository.java
+++ backend/src/main/java/br/com/julianocanuto/vendas/repositories/SaleRepository.java
@@ -11,6 +11,6 @@ import br.com.julianocanuto.vendas.entities.Sale;
 public interface SaleRepository extends JpaRepository<Sale, Long>{
 
 	@Query("SELECT new br.com.julianocanuto.vendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
-			+ " FROM Sale AS obj GROUPBY obj.seller")
+			+ " FROM Sale AS obj GROUP BY obj.seller")
 	List<SaleSumDTO> amountOfSalesGroupedBySeller();
 }
