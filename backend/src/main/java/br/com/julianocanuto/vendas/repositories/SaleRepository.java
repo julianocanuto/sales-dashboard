e49922a685193b0039a3649b86eb7ee1ee6961a2
Juliano Canuto <julianocanuto@gmail.com>
2021-09-12 21:09:29 -0300

    Add custom query amount of sales grouped by seller in SaleRepository

--- backend/src/main/java/br/com/julianocanuto/vendas/repositories/SaleRepository.java
+++ backend/src/main/java/br/com/julianocanuto/vendas/repositories/SaleRepository.java
@@ -1,9 +1,16 @@
 package br.com.julianocanuto.vendas.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import br.com.julianocanuto.vendas.dto.SaleSumDTO;
 import br.com.julianocanuto.vendas.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>{
 
+	@Query("SELECT new br.com.julianocanuto.vendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+			+ " FROM Sale AS obj GROUPBY obj.seller")
+	List<SaleSumDTO> amountOfSalesGroupedBySeller();
 }
